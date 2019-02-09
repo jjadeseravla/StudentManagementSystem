80d64c5f5505548324640680899e044647ae1b88
jade <jadealvares@hotmail.co.uk>
2019-02-09 12:16:03 +0000

    written new methods in School class

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Money Management System for a school for teachers to keep track of
 
 Every student needs to pay a total of £10,000, and the fee paid initially is 0.
 
+## To Run:
+
+Navigate to System folder and run:
+``` java FILENAME.java ```
 
 ## Planning:
 School:
@@ -27,8 +31,3 @@ student:
 - grade
 - fees paid
 - fees total
-
-## To Run:
-
-Navigate to System folder and run:
-``` java FILENAME.java ```
--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -3,11 +3,55 @@ package school.management.system;
 //a school has many teachers and many students
 //so ArrayList implements the many students and teachers
 
+import java.util.List;
+
 public class School {
 
-    private Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers;
+    private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
 
+    //created when a new school object is created
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    //returns list of teachers in the school
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    //adds a teacher to the school
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void updateTotalMoneyEarned(int moneyEarned) {
+        totalMoneyEarned += moneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    //update the money that is spent by the school, which is only the money it uses to pay the salary of teachers
+    public void updateTotalMoneySpent(int moneySpent) {
+        totalMoneyEarned -= moneySpent;
+    }
 }
